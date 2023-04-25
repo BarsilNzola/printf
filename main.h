21bcd9ba7b40b9b7cbdb629b709f4e0e06d7e858
BarsilNzola <barsiljohn@gmail.com>
2023-04-25 11:10:35 -0700

    with a face like mine, I do better in print

--- main.h
+++ main.h
@@ -4,7 +4,28 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct format_t - struct for format specifiers
+ * @spec: format specifier
+ * @f: function pointer to handle specifier
+ */
+typedef struct format_t
+{
+	char *spec;
+	int (*f)(va_list);
+} format_t;
+
 int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
+int print_u(va_list args);
+int print_o(va_list args);
+int print_x(va_list args);
+int print_X(va_list args);
+int print_p(va_list args);
+int print_b(va_list args);
 
 #endif /* MAIN_H */
 
