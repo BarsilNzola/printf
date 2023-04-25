ac78bbe7613c6450e90c99cb310c2faa094a583f
BarsilNzola <barsiljohn@gmail.com>
2023-04-25 06:15:17 -0700

    education is when you read the fine print

--- main.h
+++ main.h
@@ -4,6 +4,11 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_char(va_list args);
+int _print_string(va_list args);
+int _print_percent(va_list args);
+int _print_int(va_list args);
 
 #endif /* MAIN_H */
 
