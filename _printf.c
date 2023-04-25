eca7043f640db556654faa4cddbaef2f50c8ca39
BarsilNzola <barsiljohn@gmail.com>
2023-04-25 05:23:15 -0700

    corrections

--- _printf.c
+++ _printf.c
@@ -1,51 +1,54 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _printf - produces output according to a format
+ * @format: character string containing zero or more directives
  *
- * Return: number of chars printed.
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	va_list args;
+	int printed_chars = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	va_start(args, format);
+
+	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			format++;
+
+			switch (*format)
+			{
+				case 'c':
+					printed_chars += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					printed_chars += _puts(va_arg(args, char *));
+					break;
+				case '%':
+					printed_chars += _putchar('%');
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					printed_chars += 2;
+					break;
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+
+		format++;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+
+	va_end(args);
+
+	return (printed_chars);
 }
+
